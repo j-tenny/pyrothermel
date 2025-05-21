85dca1678af03b43b6e47079d3bc9eac573993fa
Johnathan Tenny <107655580+j-tenny@users.noreply.github.com>
2025-05-20 17:33:16 -0700

    Revert "Change packaging setup"
    This reverts commit f683e41071d990c1a3a6a5ae318d7da4b59e902b.

--- setup.py
+++ setup.py
@@ -1,24 +1,55 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 import glob
-import pybind11
 
-class get_pybind_include:
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path"""
+    def __init__(self, user=False):
+        self.user = user
+
     def __str__(self):
-        return pybind11.get_include()
+        import pybind11
+        return pybind11.get_include(self.user)
 
-sources = ['pyrothermel/pyrothermel_bindings.cpp'] + glob.glob('src/behave/*.cpp')
+# Include all .cpp files in src/behave
+behave_sources = glob.glob('src/behave/*.cpp')
 
 ext_modules = [
     Extension(
-        "pyrothermel_bindings",
-        sources=sources,
+        'pyrothermel_bindings',  # Ensure this matches your package structure
+        sources=['pyrothermel/pyrothermel_bindings.cpp'] + behave_sources,
         include_dirs=[
             get_pybind_include(),
+            get_pybind_include(user=True),
+            'pybind11/include',
             'src/behave',
         ],
-        language="c++",
+        language='c++',
     ),
 ]
 
-setup(ext_modules=ext_modules, cmdclass={"build_ext": build_ext})
+
+
+
+# Setup
+setup(
+    name='pyrothermel',
+    version='0.0.5',
+    author='Johnathan Tenny',
+    author_email='jt893@nau.edu',
+    description='Python bindings and API for Behave extended Rothermel fire models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/j-tenny/pyrothermel',
+    license='MIT',
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C++',
+        'Operating System :: OS Independent',
+    ],
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
+)
